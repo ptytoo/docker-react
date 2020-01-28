dcd59ce6b128451fd4e155548e805c59386647bf
ptytoo <ptytoo@naver.com>
2020-01-28 11:16:17 +0900

    add text App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was changed on the Feature branch
+          I was changed on the Feature branch~
         </p>
         <a
           className="App-link"
